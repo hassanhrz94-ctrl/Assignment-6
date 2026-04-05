9e905d08a39caf8613bedbfb3d05e7626dcb92f5
Hassan <hassan@gmail.com>
2026-04-05 09:16:41 +0600

    tab switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Footer from './component/footer/Footer'
 
 import Navbar from './component/navbar/Navbar'
 import Products from './component/products/Products'
+import Cart from './component/cart/Cart'
 
 
 const productsPromise = async()=>{
@@ -12,16 +13,30 @@ const productsPromise = async()=>{
   return res.json();
 
 }
-const productive=productsPromise()
+const productive=productsPromise();
 
 function App() {
+  
+  const[isActiveTab,setActiveTab] =useState('product')
+  console.log(isActiveTab)
 
 
   return (
     <>
      <Navbar/>
 
-    <Suspense fallback={<h1>loading..</h1>}> <Products productive={productive}/></Suspense>
+
+     {/* name of each tab group should be unique */}
+<div className="tabs tabs-box">
+  <input type="radio" name="my_tabs_1" className="tab" aria-label="Products" defaultChecked onClick={()=>setActiveTab('product')}/>
+  <input type="radio" name="my_tabs_1" className="tab" aria-label="Cart" onClick={()=>setActiveTab('cart')}  />
+
+</div>
+
+    <Suspense fallback={<h1>loading..</h1>}> {isActiveTab === 'product' && <Products productive={productive}/>}</Suspense>
+    
+
+    {isActiveTab === 'cart' && <Cart/>}
      
 <Footer/>
      
